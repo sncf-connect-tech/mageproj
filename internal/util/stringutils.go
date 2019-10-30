234da2303c2fa5dbab44af2cbe7857a35b63c303
Nicolas Ocquidant <nocquidant@oui.sncf>
2019-10-30 11:03:26 +0100

    Improve lisibility

--- internal/util/stringutils.go
+++ internal/util/stringutils.go
@@ -6,7 +6,7 @@ import "strings"
 func TrimString(val string) string {
 	val = strings.TrimSpace(val)
 	val = strings.TrimPrefix(val, "\"")
-	val = strings.TrimSuffix(val, "\n")
 	val = strings.TrimSuffix(val, "\"")
+	val = strings.TrimSuffix(val, "\n")
 	return val
 }
