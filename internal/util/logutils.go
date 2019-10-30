be51f291564114f077cf6037ac1b44f93bf9558d
Nicolas Ocquidant <nocquidant@oui.sncf>
2019-10-30 11:03:04 +0100

    Logger is to stderr

--- internal/util/logutils.go
+++ internal/util/logutils.go
@@ -7,7 +7,7 @@ import (
 
 // by default, the log stdlib package will be set to discard output.
 // Running with mage -v will set the output to stdout.
-var logger = log.New(os.Stdout, "", 0)
+var logger = log.New(os.Stderr, "", 0)
 
 // LogIfVerbose logs message if verbose flag is on.
 func LogIfVerbose(msg string, v ...interface{}) {
